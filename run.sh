59aa4d1dc3ee752e1742b6c91b1d1daf7cde0fd2
Wichayaporn Wongkamjan <wwongkam@cmlsub00.umiacs.umd.edu>
2021-11-15 22:50:40 -0500

    out dir update

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=mbpo_hopper_300                              # sets the job name
+#SBATCH --output=out/hopper_out.%j                            # indicates a file to redirect STDOUT to; %j is the jobid 
+#SBATCH --error=out/hopper_err_out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
+#SBATCH --time=48:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --qos=medium                                          # set QOS, this will determine what resources can be requested
+#SBATCH --mem=16GB                                              # memory required by job; if unit is not specified MB will be assumed
+#SBATCH --gres=gpu:1
+#SBATCH --account=furongh
+#SBATCH --mail-user=wwongkam@terpmail.umd.edu
+#SBATCH --mail-type=ALL
+module add cuda/10.2.89
+module load cudnn/v8.2.1
+
+srun bash -c "hostname; python main_mbpo.py --env_name 'Hopper-v2' --num_epoch 300 --model_type 'pytorch'"
+# once the end of the batch script is reached your job allocation will be revoked
+
+
